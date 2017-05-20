325b828f9c42b38c74ecc6729f0564e28fef72c1
lishniii <lishni.tilakaratne@gmail.com>
2017-05-20 20:50:20 +0530

    Classifier moved to different file

--- index.php
+++ index.php
@@ -13,59 +13,7 @@
     }
 </style>
 
-<?php
-require_once('TwitterAPIExchange.php');
-
-/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
-$settings = array(
-    'oauth_access_token' => "227584299-aw6cuhfYfe4yLsRrFWqQSzkrQjkFc60hTceKthxm",
-    'oauth_access_token_secret' => "MucbkDcIGhafD4KM7C5BY9FXyWy4Rs5wU8mff39fQFhuU",
-    'consumer_key' => "EVO4y3SAXkEARVD7TQ4teV5Df",
-    'consumer_secret' => "cO9MTPdrhC1otgQoGs6oOdURuD8KKMNv5ujTwDKrpYZPimTO0E"
-);
-
-$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
-$requestMethod = "GET";
-if (isset($_GET['user']))  {$user = $_GET['user'];}  else {$user  = "samjancool";}
-if (isset($_GET['count'])) {$count = $_GET['count'];} else {$count = 500;}
-if (isset($_GET['include_rts'])) {$include_rts = $_GET['include_rts'];} else {$include_rts = false;}
-if (isset($_GET['exclude_replies'])) {$exclude_replies = $_GET['exclude_replies'];} else {$exclude_replies = 1;}
-$getfield = "?screen_name=$user&count=$count&include_rts=$include_rts&exclude_replies=$exclude_replies";
-$twitter = new TwitterAPIExchange($settings);
-$string = json_decode($twitter->setGetfield($getfield)
-    ->buildOauth($url, $requestMethod)
-    ->performRequest(),$assoc = TRUE);
-
-$allTweets = [];
-$userDetails = [];
-foreach($string as $items)
-{
-    $userDetails = $items['user'];
-    array_push($allTweets, $items['text']);
-}
-
-//MonkeyLearn Functions
-require 'monkeylearn/autoload.php';
-
-$ml = new MonkeyLearn\Client('81137672d671ea08af4938ad6a970f4f7bdc8a21');
-$module_id = 'cl_5icAVzKR'; //https://app.monkeylearn.com/main/classifiers/cl_YmN3QwVL/
-$res = $ml->classifiers->classify($module_id, $allTweets, true);
-
-$result = $res->result;
-$keywords = [];
-foreach($result as $items)
-{
-    if (!in_array($items[0]["label"], $keywords))
-    {
-        array_push($keywords, $items[0]["label"]);
-    }
-
-}
-
-//var_dump($keywords);
-
-
-?>
+<?php include 'Classifier.php';?>
 
 
 
